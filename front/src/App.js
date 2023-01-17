6a1e7796c638d45185ca607efaf56d24e58a189d
andreicotoi <cotoiandreigeorgel@gmail.com>
2023-01-17 04:35:36 +0200

    updates

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Login from './components/Login'
-import Register from './components/Register'
+import Register from './components/Login'
 import Home from './components/Home'
 
 function App() {
